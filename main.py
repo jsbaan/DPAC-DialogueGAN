03ecdf7cac29d8519cec1f17550ccb42cf798378
petraormel <p_pinoa@hotmail.com>
2018-12-19 16:52:18 +0100

    fix

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ MAX_SEQ_LEN = 20
 BATCH_SIZE = 64
 MLE_TRAIN_EPOCHS = 100
 ADV_TRAIN_EPOCHS = 50
-DIS_TRAIN_EPOCHS = 2
+DIS_TRAIN_EPOCHS = 1
 
 GEN_EMBEDDING_DIM = 256
 GEN_HIDDEN_DIM = 256
@@ -325,12 +325,12 @@ def pre_train_discriminator(dis, dis_opt, gen, corpus, epochs):
             dis_opt.step()
 
             
-            plt.plot(real_list, label='real')
-            plt.plot(fake_list, label='fake')
-            plt.legend()
-            plt.savefig('rewards.png')
+    plt.plot(real_list, label='real')
+    plt.plot(fake_list, label='fake')
+    plt.legend()
+    plt.savefig('rewards.png')
 
-            sys.exit()
+
 
     torch.save(dis.state_dict(), "discriminator_final.pth.tar")
     print(real_r, "Real")
